5e7e4a52aa2bccea966800fa863cdd1fb7095d55
anani <abood.anani57@gmail.com>
2022-07-09 21:48:16 +0300

    first commit

---
+++ src/Components/Player/Player.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class Player extends React.Component {
+    render(){
+        return(
+            <iframe title="song"  src="https://open.spotify.com/embed/track/4gagfSo6yKtWXDicu2oIcr?utm_source=generator" width="100%" height="380" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
+        )
+    }
+}
+
+export default Player;
